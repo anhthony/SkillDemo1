d2b171b97ff7b5f56a7d48e505601365a93ac318
anhthony <a1ton@ucsd.edu>
2022-04-28 16:55:54 -0700

    Updates

--- Demo1.java
+++ Demo1.java
@@ -1,1 +1,18 @@
+public class Demo1
+{
+    static int sumIntArray(int[] arr)
+    {
+        int sum = 0;
+        for(int num : arr)
+        {
+            sum += num;
+        }
+        return sum;
+    }
 
+    public static void main(String[] args)
+    {
+        int[] arr1 = {1,2,3,4};
+        System.out.println(sumIntArray(arr1));
+    }
+}
---
+++ Demo1Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Demo1Test {
+    
+    @Test
+    public void testSum()
+    {
+        int[] arr1 = {1,2,3,4};
+        assertEquals(9, Demo1.sumIntArray(arr1));
+    }
+}
